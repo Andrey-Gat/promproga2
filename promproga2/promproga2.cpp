0bd9e7f018a64c3e7e907aa75f60d6857d560d6d
Andrey Gagalayan <gagalaan@mail.ru>
2026-05-12 18:04:42 +0300

    ispravil timura

--- promproga2/promproga2.cpp
+++ promproga2/promproga2.cpp
@@ -1,14 +1,14 @@
 ﻿#include <stdio.h>
 #define _CRT_SECURE_NO_
 void say() {
-    printf("viva Barca");
+    printf("visca Barca");
 }
 void salam() {
     printf("salam brat\n");
 }
 int main()
 {
-    printf("Vsem privet\n");
+    printf("Vsem privet!\n");
     salam();
     printf("kto ne riskyet tot ne piet shampanskoe");
 }
