ef5774d93abc6caa3897969ada96339e8b1ea437
Andrey Gagalayan <gagalaan@mail.ru>
2026-05-12 18:22:00 +0300

    Alpha: change greeting message

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_
+void say() {
+    printf("visca Barca");
+}
+void salam() {
+    printf("salam brat\n");
+}
+int main()
+{
+    printf("ALPHA: Vsem privet\n");
+    salam();
+    printf("kto ne riskyet tot ne piet shampanskoe");
+}
